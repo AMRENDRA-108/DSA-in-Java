3e65cbb968fcf13747342b9adaa9a77688fda214
Amrendra Kumar <kumaramrendra1512023@gmail.com>
2026-08-05 03:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (81.87%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -5,6 +5,9 @@ class Solution {
         
 
         for(int i = 0 ; i < n; i++ ){
+
+            // if  n= 3, yhan array ka length 6 hai but ham for loop n = 2 tak hi run krange 
+            // kyonki i = 3 pe ya 3 se bra no pe ans[2*3] = ans[6] - 6 is wrong index
             ans[ 2*i] = nums[i];
             ans[2*i+1] = nums[i + n];
         }
