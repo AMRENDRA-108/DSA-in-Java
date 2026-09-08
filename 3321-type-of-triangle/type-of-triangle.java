2f9fc38116a5cef71c77614902f996b41470b600
Amrendra Kumar <kumaramrendra1512023@gmail.com>
2026-09-08 17:39:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (39.34%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -5,8 +5,12 @@ class Solution {
         int b = nums[1];
         int c = nums[2];
 
-        if(a + b <= c || a + c <= b || b + c <=  a){
+        // if(a + b <= c || a + c <= b || b + c <=  a){
 
+        //     return "none";
+        // }
+
+        if(!(a + b > c && b + c > a && a + c > b) ){
             return "none";
         }
 
