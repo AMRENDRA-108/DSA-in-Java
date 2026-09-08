e97cb95807ecb66fe5b49da1ad752a92bcf0bd19
Amrendra Kumar <kumaramrendra1512023@gmail.com>
2026-09-08 16:12:21 +0530

    Time: 1 ms (93.62%) | Memory: 42.7 MB (42.46%) - LeetSync

--- 3830-find-closest-person/find-closest-person.java
+++ 3830-find-closest-person/find-closest-person.java
@@ -1,8 +1,18 @@
 class Solution {
     public int findClosest(int x, int y, int z) {
 
-        int dist1 = Math.abs(z - x);
-        int dist2 = Math.abs(z - y);
+        //int dist1 = Math.abs(z - x);
+        //int dist2 = Math.abs(z - y);
+
+        // Another way without using inbuilt function
+
+        int dist1 = z - x;
+        int dist2 = z - y;
+
+       // making positive answer without using inbuilt function
+
+       dist1 =  dist1 < 0 ? -dist1 : dist1;
+       dist2 =  dist2 < 0 ? -dist2 : dist2;
 
        return dist1 < dist2 ? 1 : dist1 > dist2 ? 2 : 0;
         
