17c17cde663b4619e633486ad45c4b84b8d1f834
Amrendra Kumar <kumaramrendra1512023@gmail.com>
2026-09-11 03:01:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (11.49%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-
+        // if num is 0 then it is always true
          if(num == 0) {
            return true;
          }
-
+        // if last digit contains 0 then their reverse will never equal to the original
          if(num % 10 == 0){
            return false;
          }
-
+        
+        // otherwise after two reversal all numbers will be same as original
         return true;   
         
     }
