1bde544313ba98e0533fccd87acee18207dc5fee
Amrendra Kumar <kumaramrendra1512023@gmail.com>
2026-09-12 23:57:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (26.27%) - LeetSync

--- 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.java
+++ 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
         
-        return (arrivalTime + delayedTime) % 24 ;
+        return (arrivalTime + delayedTime) % 24  ;
     }
 }
